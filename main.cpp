674915a35bc3bacf3d8e595d123e38a84d732623
abdulloh666 <narcos765@gmail.com>
2022-09-07 23:33:19 +0500

    C++

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 16;
+    string name = "Abdulloh";
+    string surname = "Ahmadjonov";
+    bool status = true;
+    double score = 7.5;
+
+
+    cout << "My name is " << name << endl;
+    cout << "My surname is " << surname << endl;
+    cout << "I'm " << age << " years old " << endl ;
+    cout << "I'm a student : " << status << endl;
+    cout << score << " is my IELST target score.";
+    return 0;
+}
